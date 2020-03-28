93127329a658a8d1625d4f4fbacf085034fec39f
Jon Chan <me@jonhmchan.com>
2020-03-27 17:52:32 -0700

    Basic scaffolding for static files for team editing page

---
+++ static/js/teams/edit.js
@@ -0,0 +1,13 @@
+// Your code starts here
+$(document).ready(function() {
+    $.ajax({
+        method: "GET",
+        url: "/api" + window.location.pathname.replace("/edit", ""),
+        success: function(data) {
+
+            // Update back link so it goes to the team page
+            $(".back").attr("href", `/teams/${data.id}`);
+
+        }
+    })
+})
