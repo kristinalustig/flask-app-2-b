81539616b6cf79c8d3e354b9c03813b05f46f577
kristinalustig <kristina.lustig@gmail.com>
2020-03-30 15:20:05 -0400

    Wrote the js that populates the details page

--- static/js/teams/detail.js
+++ static/js/teams/detail.js
@@ -1,1 +1,36 @@
 // Your code starts here
+$(document).ready(function() {
+
+    $.ajax({
+        method: "GET",
+        url: "../api/" + window.location.pathname,
+        success: function(data) {
+            teamName = `<h1>${data.name}</h1>`;
+            teamDescription = `<p>${data.description}</p>`;
+            $(".js-header")
+                .append(teamName)
+                .append(teamDescription);
+            pokemonList = data.members;
+            $.ajax({
+                method: "GET",
+                url: "../api/pokemon",
+                success: function(pokemonData) {
+
+                    for (x = 0; x < pokemonList.length; x++) {
+                        currentPokemon = pokemonList[x].pokemon_id;
+                        $pokemonRow = 
+                        `<tr>
+                            <td><img class="poke-image" src=${pokemonData[currentPokemon-1].image_url}></td>
+                            <td><a href="/pokemon/${currentPokemon}">${pokemonData[currentPokemon-1].name}</a></td>
+                            <td>${pokemonList[x].level}</td>
+                            <td>${pokemonData[currentPokemon-1].types}</td>
+                        </tr>`;
+                        $(".js-teams-pokemon").append($pokemonRow);
+                    }
+                }
+            })
+            
+        }
+    });
+
+});
